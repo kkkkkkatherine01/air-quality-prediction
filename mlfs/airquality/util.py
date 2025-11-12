12a8cd1329acc4401b22a068121b8ca0a417ffde
Yinyin Kong <yinyink@kth.se>
2025-11-13 00:14:10 +0100

    main

--- mlfs/airquality/util.py
+++ mlfs/airquality/util.py
@@ -291,10 +291,26 @@ def backfill_predictions_for_monitoring(weather_fg, air_quality_df, monitor_fg, 
     features_df = weather_fg.read()
     features_df = features_df.sort_values(by=['date'], ascending=True)
     features_df = features_df.tail(10)
-    features_df['predicted_pm25'] = model.predict(features_df[['temperature_2m_mean', 'precipitation_sum', 'wind_speed_10m_max', 'wind_direction_10m_dominant']])
+
+    # 合并air_quality_df，获取lag特征
+    features_df = pd.merge(
+        features_df, 
+        air_quality_df[['date', 'pm25_lag_1', 'pm25_lag_2', 'pm25_lag_3']], 
+        on='date', 
+        how='left'
+    )
+
+    # 预测
+    features_df['predicted_pm25'] = model.predict(
+        features_df[['pm25_lag_1', 'pm25_lag_2', 'pm25_lag_3',
+                     'temperature_2m_mean', 'precipitation_sum', 
+                     'wind_speed_10m_max', 'wind_direction_10m_dominant']]
+    )
+    #features_df['predicted_pm25'] = model.predict(features_df[['temperature_2m_mean', 'precipitation_sum', 'wind_speed_10m_max', 'wind_direction_10m_dominant']])
     df = pd.merge(features_df, air_quality_df[['date','pm25','street','country']], on="date")
     df['days_before_forecast_day'] = 1
     hindcast_df = df
     df = df.drop('pm25', axis=1)
+    df = df.drop(columns=['pm25_lag_1', 'pm25_lag_2', 'pm25_lag_3'])
     monitor_fg.insert(df, write_options={"wait_for_job": True})
     return hindcast_df
